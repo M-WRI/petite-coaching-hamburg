1b920ee451608056f4917d038e0d08ab0e785493
Moritz Wright <me@moritzwright.com>
2022-01-04 16:15:25 -0600

    early idendification section with animation

---
+++ components/EarlyIdentification.js
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { useAnimation, motion } from "framer-motion";
+import Image from "next/image";
+
+// style
+import style from "../styles/EarlyIdentification.module.css";
+
+const EarlyIdentification = () => {
+  const { ref, inView } = useInView({
+    threshold: 0.8,
+  });
+
+  const animation = {
+    imageOne: {
+      opacity: 1,
+      y: "0%",
+      transition: { bounce: 0, ease: "easeOut", duration: 0.5 },
+    },
+
+    imageTwo: {
+      opacity: 1,
+      y: "0%",
+      transition: { bounce: 0, ease: "easeOut", duration: 0.5, delay: 0.3 },
+    },
+
+    box: {
+      opacity: 1,
+      transition: { bounce: 0, ease: "easeOut", duration: 0.5, delay: 0.7 },
+    },
+  };
+
+  const imgOne = useAnimation();
+  const imgTwo = useAnimation();
+  const contBox = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      imgOne.start("imageOne");
+      imgTwo.start("imageTwo");
+      contBox.start("box");
+    }
+  }, [inView]);
+
+  return (
+    <div ref={ref} className={style.container}>
+      <motion.div
+        className={style.image}
+        initial={{ opacity: 0, y: "100%" }}
+        animate={imgOne}
+        variants={animation}
+      ></motion.div>
+      <div className={style.contentContainer}>
+        <motion.div
+          className={style.contentInformation}
+          initial={{ opacity: 0 }}
+          animate={contBox}
+          variants={animation}
+        >
+          <h2 className={style.contentHead}>Frühförderung</h2>
+          <p className={style.contentText}>
+            Jedes Kind entwickelt sich einzigartig Wir entwickeln eine
+            einzigartige Förderung
+          </p>
+          <Image
+            src="/images/arrow-down.svg"
+            alt="logo"
+            width={36}
+            height={36}
+          />
+        </motion.div>
+        <motion.div
+          className={style.contentImage}
+          initial={{ opacity: 0, y: "100%" }}
+          animate={imgTwo}
+          variants={animation}
+        ></motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default EarlyIdentification;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 // components
+import EarlyIdentification from "../components/EarlyIdentification";
 import LandingSection from "../components/LandingSection";
 
 export default function Home() {
   return (
     <main style={{ position: "relative", zIndex: 1 }}>
       <LandingSection />
+      <EarlyIdentification />
     </main>
   );
 }
