7853114a30ee12ca27bd7029cd323c10b8a1b842
Moritz Wright <me@moritzwright.com>
2022-01-07 21:30:13 -0600

    starting animation for section

--- components/ExpertiseSection.js
+++ components/ExpertiseSection.js
@@ -1,6 +1,9 @@
 // style
 import style from "../styles/ExpertiseSection.module.css";
 import LinkBox from "./LinkBox";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 const ExpertiseSection = () => {
   const data = [
@@ -13,9 +16,34 @@ const ExpertiseSection = () => {
     { id: 2, title: "Babykurse", slug: "#", img: "/images/babykurse-1.jpg" },
   ];
 
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+
+  const animation = {
+    content: {
+      opacity: 1,
+      transition: { bounce: 0, ease: "easeOut", duration: 0.5 },
+    },
+  };
+
+  const animationControl = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animationControl.start("content");
+    }
+  }, [inView, animationControl]);
+
   return (
     <section className={style.container}>
-      <div className={style.contentContainer}>
+      <motion.div
+        ref={ref}
+        className={style.contentContainer}
+        initial={{ opacity: 0 }}
+        animate={animationControl}
+        variants={animation}
+      >
         <h2 className={style.contentHead}>Meine Expertise</h2>
         <p className={style.contentText}>
           Ich arbeite nach den neusten wissenschaftlichen Erkenntnissen zum
@@ -23,7 +51,7 @@ const ExpertiseSection = () => {
           lebenslanger Prozess – daher bilde ich mich regelmäßig weiter und
           lerne täglich dazu.
         </p>
-      </div>
+      </motion.div>
       {data.map((link) => (
         <LinkBox
           key={link.id}
