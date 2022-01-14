9d4f0f573f7456c701365cea56c7e9937075b422
Moritz Wright <me@moritzwright.com>
2022-01-14 11:14:17 -0600

    expertise section done

--- components/HomeSections/ExpertiseSection.js
+++ components/HomeSections/ExpertiseSection.js
@@ -1,58 +1,31 @@
+import InfoBoxSquare from "../utils/InfoBoxSquare";
+
 // style
-import style from "../../styles/ExpertiseSection.module.css";
 import LinkBox from "../LinkBox";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
 
 const ExpertiseSection = () => {
-  const data = [
-    {
-      id: 1,
-      title: "Familiencoaching",
-      slug: "#",
-      img: "/images/familiencoaching-1.jpg",
-    },
-    { id: 2, title: "Babykurse", slug: "#", img: "/images/babykurse-1.jpg" },
-  ];
-
-  const { ref, inView } = useInView({
-    threshold: 0.5,
-  });
-
-  const animation = {
-    content: {
-      opacity: 1,
-      transition: { bounce: 0, ease: "easeOut", duration: 0.5 },
+  const data = {
+    links: [
+      {
+        id: 1,
+        title: "Familiencoaching",
+        slug: "#",
+        img: "/images/familiencoaching-1.jpg",
+      },
+      { id: 2, title: "Babykurse", slug: "#", img: "/images/babykurse-1.jpg" },
+    ],
+    info: {
+      title: "Meine Expertise",
+      text: "Ich arbeite nach den neusten wissenschaftlichen Erkenntnissen zum Thema frühkindliche Entwicklung und Förderung. Lernen ist ein lebenslanger Prozess – daher bilde ich mich regelmäßig weiter und lerne täglich dazu.",
     },
   };
 
-  const animationControl = useAnimation();
-
-  useEffect(() => {
-    if (inView) {
-      animationControl.start("content");
-    }
-  }, [inView, animationControl]);
-
   return (
-    <section className={style.container}>
-      <motion.div
-        ref={ref}
-        className={style.contentContainer}
-        initial={{ opacity: 0 }}
-        animate={animationControl}
-        variants={animation}
-      >
-        <h2 className={style.contentHead}>Meine Expertise</h2>
-        <p className={style.contentText}>
-          Ich arbeite nach den neusten wissenschaftlichen Erkenntnissen zum
-          Thema frühkindliche Entwicklung und Förderung. Lernen ist ein
-          lebenslanger Prozess – daher bilde ich mich regelmäßig weiter und
-          lerne täglich dazu.
-        </p>
-      </motion.div>
-      {data.map((link) => (
+    <section className="expertise-container">
+      <div className="expertise-infobox">
+        <InfoBoxSquare title={data.info.title} text={data.info.text} />
+      </div>
+      {data.links.map((link) => (
         <LinkBox
           key={link.id}
           title={link.title}
--- components/utils/Text.js
+++ components/utils/Text.js
@@ -2,7 +2,6 @@
 import style from "../../styles/Text.module.css";
 
 const Text = ({ children, center }) => {
-  console.log(center);
   return (
     <p style={{ textAlign: center ? "center" : "left" }} className={style.text}>
       {children}
