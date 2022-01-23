5f8c7ecc16c3ecfeb7180d46a26995df12b2bff0
Moritz Wright <me@moritzwright.com>
2022-01-23 15:56:58 -0600

    contact page screen done

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import {
   AiOutlineMail,
   AiOutlinePhone,
@@ -10,53 +11,40 @@ import style from "../styles/Kontakt.module.css";
 
 const kontakt = () => {
   return (
-    <div className={style.container}>
+    <section className={style.container}>
       <div className={style.wrapper}>
-        <div className={style.contactContainer}>
-          <h3 className={style.footerName}>Teresa Jäger</h3>
-          <div className={style.iconsContainer}>
-            <div className={style.iconWrapper}>
-              <AiOutlineMail />
-            </div>
-
-            <div className={style.iconWrapper}>
-              <AiOutlinePhone />
-            </div>
-
-            <div className={style.iconWrapper}>
-              <AiOutlineInstagram />
-            </div>
-
-            <div className={style.iconWrapper}>
-              <AiOutlineLinkedin />
-            </div>
-          </div>
-        </div>
         <div className={style.formContainer}>
           <form>
-            <div className={style.name}>
-              <input type="text" placeholder="Vorname" />
-              <input type="text" placeholder="Nachname" />
+            <input className={style.input} type="text" placeholder="Name" />
+            <div className={style.inputWrapper}>
+              <input className={style.input} type="text" placeholder="Email" />
+              <input
+                className={style.input}
+                type="text"
+                placeholder="Telefon"
+              />
             </div>
-            <div className={style.name}>
-              <input type="text" placeholder="Email" />
-              <input type="text" placeholder="Telefon" />
-            </div>
-            <div className={style.inputContainer}>
-              <div className={style.select}>
-                <input type="radio" value="Male" name="service" /> Frühförderung
-              </div>
-              <div className={style.select}>
-                <input type="radio" value="Male" name="service" />{" "}
+            <div className={style.controlGroup}>
+              <label className={`${style.control} ${style.controlRadio}`}>
+                Frühförderung
+                <input
+                  type="radio"
+                  name="radio"
+                  value="frühförderung"
+                  checked="checked"
+                />
+                <div className={style.controlIndicator}></div>
+              </label>
+
+              <label className={`${style.control} ${style.controlRadio}`}>
                 Familiencoaching
-              </div>
-              <div className={style.select}>
-                <input type="radio" value="Male" name="service" /> Babykurse
-              </div>
+                <input type="radio" name="radio" value="familiencoaching" />
+                <div className={style.controlIndicator}></div>
+              </label>
             </div>
             <textarea
               className={style.textarea}
-              rows="10"
+              rows="1"
               placeholder="Nachricht..."
             ></textarea>
             <button className={style.button} type="submit">
@@ -64,8 +52,54 @@ const kontakt = () => {
             </button>
           </form>
         </div>
+        <div className={style.contactContainer}>
+          <h3 className={style.footerName}>Teresa Jäger</h3>
+          <div className={style.iconsContainer}>
+            <Link href="mailto:hello@petite-coaching-hamburg.de">
+              <a>
+                <div className={style.iconWrapper}>
+                  <AiOutlineMail />
+                </div>
+                hello@petite-coaching-hamburg.de
+              </a>
+            </Link>
+
+            <Link
+              href="https://www.instagram.com/petite.coaching.hamburg/"
+              passHref={true}
+            >
+              <a target="_blank">
+                <div className={style.iconWrapper}>
+                  <AiOutlineInstagram />
+                </div>
+                @petite.coaching.hamburg
+              </a>
+            </Link>
+
+            <Link href="tel:+4915735361322">
+              <a>
+                <div className={style.iconWrapper}>
+                  <AiOutlinePhone />
+                </div>
+                +49 1573 53 61 32 2
+              </a>
+            </Link>
+
+            <Link
+              href="https://www.linkedin.com/in/teresa-j%C3%A4ger-773a2a1ba/"
+              passHref={true}
+            >
+              <a target="_blank">
+                <div className={style.iconWrapper}>
+                  <AiOutlineLinkedin />
+                </div>
+                Teresa Jäger
+              </a>
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
