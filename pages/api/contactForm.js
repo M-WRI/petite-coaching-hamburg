8697261c2ae4c8ba47ae89529c488c340ca2b2f3
Moritz Wright <me@moritzwright.com>
2022-01-28 12:26:29 -0600

    email bug

--- pages/api/contactForm.js
+++ pages/api/contactForm.js
@@ -2,11 +2,10 @@ const mail = require("@sendgrid/mail");
 
 mail.setApiKey(process.env.EMAIL_API_KEY);
 
-export default function (req, res) {
+export default async function (req, res) {
   const body = JSON.parse(req.body);
-  console.log(body, "<----");
-
-  const message = `
+  try {
+    const message = `
     Vielen dank für Ihre nachricht.\r\n 
     Ich werde mich in kürze bei Ihnen melden.\r\n
     \r\n
@@ -15,7 +14,7 @@ export default function (req, res) {
     Teresa Jäger - Petite Coaching Hamburg
   `;
 
-  const Adminmessage = `
+    const Adminmessage = `
     Name: ${body.name}\r\n
     Email: ${body.email}\r\n
     Telefon: ${body.phone}\r\n
@@ -23,23 +22,27 @@ export default function (req, res) {
     Nachricht: ${body.message}\r\n
 `;
 
-  const data = {
-    to: `${body.email}`,
-    from: "hello@petite-coaching-hamburg.de",
-    subject: `Anfragebestätigung - Petite Coaching Hamburg`,
-    text: message,
-    html: message.replace(/\r\n/g, "<br>"),
-  };
+    const data = {
+      to: `${body.email}`,
+      from: "hello@petite-coaching-hamburg.de",
+      subject: `Anfragebestätigung - Petite Coaching Hamburg`,
+      text: message,
+      html: message.replace(/\r\n/g, "<br>"),
+    };
 
-  const Admindata = {
-    to: "hello@petite-coaching-hamburg.de",
-    from: "kontakt-formular@petite-coaching-hamburg.de",
-    subject: `${body.name} – ${body.reason}`,
-    text: Adminmessage,
-    html: Adminmessage.replace(/\r\n/g, "<br>"),
-  };
+    const Admindata = {
+      to: "hello@petite-coaching-hamburg.de",
+      from: "kontakt-formular@petite-coaching-hamburg.de",
+      subject: `${body.name} – ${body.reason}`,
+      text: Adminmessage,
+      html: Adminmessage.replace(/\r\n/g, "<br>"),
+    };
 
-  mail.send(data);
-  mail.send(Admindata);
+    mail.send(data);
+    mail.send(Admindata);
+  } catch (err) {
+    console.log(err);
+  }
+
   res.status(200).json({ status: "ok" });
 }
